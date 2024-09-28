0a16b32eecc8a2198f30d74158c479c83797736e
Blessing <bqdithole22@live.com>
2024-09-28 02:09:08 +0200

    task 3

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -48,3 +48,42 @@ class DB:
         self._session.commit()
 
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Finds a user by arbitrary keyword arguments.
+        Args:
+            **kwargs: Arbitrary keyword arguments for filtering.
+        Returns:
+            User: The first user found that matches the criteria.
+        Raises:
+            NoResultFound: If no user is found.
+            InvalidRequestError: If invalid arguments are provided.
+        """
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound("No user found")
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        Update a user's attributes.
+        Args:
+            user_id (int): The ID of the user to update.
+            **kwargs: Arbitrary keyword arguments of the attributes to update.
+        Returns:
+            None: The method doesn't return anything.
+        Raises:
+            ValueError: If any attribute provided in kwargs does not
+            exist in the User model.
+        """
+        try:
+            user = self.find_user_by(id=user_id)
+            for key, value in kwargs.items():
+                if hasattr(user, key):
+                    setattr(user, key, value)
+                else:
+                    raise InvalidRequestError
+            self._session.commit()
+        except (NoResultFound, InvalidRequestError, ValueError):
+            raise ValueError
